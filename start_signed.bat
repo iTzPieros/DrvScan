ed7fd573a0b365ee0e56fd4f9156c84d110938f2
Pieros <pierosdidio227@gmail.com>
2026-08-10 12:18:44 +0200

    Add files via upload

--- start_signed.bat
+++ start_signed.bat
@@ -1,6 +1,5 @@
-sc create rtcoredriver binPath="%~dp0\x64\Release\rtcore.sys" type=kernel
-sc start rtcoredriver
+sc create rtcorevln binPath="%~dp0\x64\Release\rtcore.sys" type=kernel
+sc start rtcorevln
 pause
-sc stop rtcoredriver
-sc delete rtcoredriver
-
+sc stop rtcorevln
+sc delete rtcorevln
